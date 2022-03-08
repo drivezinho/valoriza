d5d520401227ce39d2539e34d122f3fea3e72ad9
renan <renan_18_cg@outlook.com>
2022-03-08 19:19:57 -0300

    Compliments

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,18 @@ import { CreateUserController } from "./controller/CreateUserController";
 import { CreateTagController } from "./controller/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureadmin";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreateComplimentController } from "./controller/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", ensureAdmin, createUserController.handle);
 router.post("/tags", createTagController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("/compliment", createComplimentController.handle);
 
 export { router };
